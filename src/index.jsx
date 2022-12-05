ef73765d12ddb3d2cfd0f473ac40395c4901ba8c
jihyunglee <jhlee7125@gmail.com>
2022-12-05 21:20:39 +0900

    add: react router 적용

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </Router>
+);
